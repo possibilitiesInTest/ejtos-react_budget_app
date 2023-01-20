779b2ea3c66b4313a37b8061dd7a3426d5fe7bed
Gourav <possibilitiesInTest@protonmail.com>
2023-01-20 18:15:49 -0500

    changes from theia to render comps

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// Add code to import the components
+import AllocationForm from './components/AllocationForm';
+import Budget from './components/Budget';
+import ExpenseItem from './components/ExpenseItem';
+import ExpenseList from './components/ExpenseList';
+import ExpenseTotal from './components/ExpenseTotal';
+import Remaining from './components/Remaining';
 import { AppProvider } from './context/AppContext';
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
-                {/* Add the code here to add the components          */}
+                
+            <AllocationForm />
+            <Budget />
+            <ExpenseItem />
+            <ExpenseList />
+            <ExpenseTotal />
+            <Remaining />
             </div>
         </AppProvider>
     );
