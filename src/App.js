b2f2d2f4fd4d2c618bc6016e2935fbe84c104ebb
Gourav <possibilitiesInTest@protonmail.com>
2023-01-21 10:45:53 -0500

    updates from theia app comps

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AllocationForm from './components/AllocationForm';
 import Budget from './components/Budget';
-import ExpenseItem from './components/ExpenseItem';
+import Currency from './components/Currency';
 import ExpenseList from './components/ExpenseList';
 import ExpenseTotal from './components/ExpenseTotal';
 import Remaining from './components/Remaining';
@@ -11,13 +11,33 @@ const App = () => {
     return (
         <AppProvider>
             <div className='container'>
-                
-            <AllocationForm />
-            <Budget />
-            <ExpenseItem />
-            <ExpenseList />
-            <ExpenseTotal />
-            <Remaining />
+                <h1 className='mt-3'>Company's Budget Allocation</h1>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
+                    <div className='col-sm'>
+                        <Currency />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Allocation</h3>
+                <div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Change allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm />
+                    </div>
+                </div>
             </div>
         </AppProvider>
     );
